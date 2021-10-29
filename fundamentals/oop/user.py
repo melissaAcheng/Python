77a085a2e16378c69d1e6463fecc5d8ac29fca91
melissaAcheng <melcheng12@gmail.com>
2021-10-29 09:12:53 -0400

    cleaned up code

--- fundamentals/oop/user.py
+++ fundamentals/oop/user.py
@@ -1,4 +1,5 @@
 class User:
+
     def __init__(self, name, email):
         self.name = name
         self.email = email
@@ -12,12 +13,16 @@ class User:
     def make_withdrawal(self, amount):
         self.account_balance -= amount
 
+    # add display balance method
     def display_user_balance(self):
         print(f"User: {self.name}, {self.account_balance}")
 
+    # add transfer amount method
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        self.display_user_balance()
+        other_user.display_user_balance()
 
 
 melissa = User("Melissa Cheng", "melcheng12@gmail.com")
@@ -28,19 +33,17 @@ melissa.make_deposit(200)
 melissa.make_deposit(200)
 melissa.make_deposit(200)
 melissa.make_withdrawal(50)
+melissa.display_user_balance()
+
 thomas.make_deposit(300)
 thomas.make_deposit(200)
 thomas.make_withdrawal(200)
+thomas.display_user_balance()
+
 katie.make_deposit(1000)
 katie.make_withdrawal(200)
 katie.make_withdrawal(40)
 katie.make_withdrawal(50)
-
-melissa.display_user_balance()
-thomas.display_user_balance()
 katie.display_user_balance()
 
 melissa.transfer_money(thomas, 50)
-
-melissa.display_user_balance()
-thomas.display_user_balance()
